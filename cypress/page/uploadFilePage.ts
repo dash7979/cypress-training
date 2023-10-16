c63c79af2d248d4f95798fbcff6e962d911c48a4
“AngelOcampo” <miguequinteroocampo@gmail.com>
2023-10-16 15:58:44 -0500

    Maaaaas errores

--- cypress/page/uploadFilePage.ts
+++ cypress/page/uploadFilePage.ts
@@ -1,13 +1,13 @@
-import { fileUploaderLocators } from "../support/fileLocators";
+import { fileLocators } from "../support/fileLocators";
 
 class UploadFilePage{
     constructor() {
     }
 
     validateFileIsUploadedSuccesfully(){        
-        cy.get(fileUploaderLocators.chooseFileButton).attachFile("dataContact.json");
-        cy.get(fileUploaderLocators.uploadButton).click()
-        cy.get(fileUploaderLocators.uploadedFileContainer).invoke('text').then((text) => {
+        cy.get(fileLocators.chooseFileButton).attachFile("dataContact.json");
+        cy.get(fileLocators.uploadButton).click()
+        cy.get(fileLocators.uploadedFileContainer).invoke('text').then((text) => {
             cy.log(text)
         })
     }
