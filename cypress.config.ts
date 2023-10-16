107346c553a0e550fafbe6d5634a5b83781c0a2d
“AngelOcampo” <miguequinteroocampo@gmail.com>
2023-10-15 20:25:53 -0500

    test de pagina con productos

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,8 +7,8 @@ export default defineConfig({
             video: false,
             screenshotOnRunFailure: false,
         setupNodeEvents(on, config) {
-            config.defaultCommandTimeout = 20000
-            config.responseTimeout = 20000
+            config.defaultCommandTimeout = 50000
+            config.responseTimeout = 50000
 
             // IMPORTANT return the updated config object
             return config;
--- cypress/e2e/demoblaze.cy.ts
+++ cypress/e2e/demoblaze.cy.ts
@@ -3,18 +3,67 @@ describe("Logg in feature", () => {
         cy.visit("https://www.demoblaze.com/index.html");
         cy.get("#login2").click();
         cy.get("#loginusername").type("Dimaria");
-        cy.wait(5500)
         cy.get("#loginpassword").type("dada");
         cy.get(".btn-primary").contains("Log in").click();
-  
         cy.get("#nameofuser").should("contain.text" , "Dimaria");
-  
-  
-        //cy.wait(2000);
-        // pasos para loguearme
+        
+
+        
+
+        cy.get(".list-group-item").contains("Phones").click();
+        cy.get(".hrefch").contains("Sony xperia z5").click();
+        cy.get(".btn-success").contains("Add to cart").click();
+        cy.get(".name").should("contain.text" , "Sony xperia z5");
+        
+
+
+        cy.get(".nav-link").contains("Home").click();
+
+
+
+        cy.get(".list-group-item").contains("Laptops").click();
+
+        cy.get(".hrefch").contains("Dell i7 8gb").click();
+        cy.get(".btn-success").contains("Add to cart").click();
+        cy.get(".name").should("contain.text" , "Dell i7 8gb");
+        
+
+        cy.get(".nav-link").contains("Home").click();
+
+
+
+        cy.get(".list-group-item").contains("Monitors").click();
+
+
+        cy.get(".hrefch").contains("Apple monitor 24").click();
+        cy.get(".btn-success").contains("Add to cart").click();
+        cy.get(".name").should("contain.text" , "Apple monitor 24");
+
+        cy.get("#cartur").contains("Cart").click();
+        cy.get(".panel-heading").should("contain.text" , "1420");
+
+        cy.get(".btn-success").contains("Place Order").click();
+        cy.get("#name").type("Dimaria");
+        cy.get("#country").type("Colombia");
+        cy.get("#city").type("La Dorada");
+        cy.get("#card").type("3925577852");
+        cy.get("#month").type("octubre");
+        cy.get("#year").type("2023");
+
+        cy.get(".btn-primary").contains("Purchase").click();
+
+        cy.get(".confirm").click();
+
+        
     });
   
     it("Validating successful login", () => {
-        //Pasos para test 1
+        
+        
     });
-  });
+
+    //it("validating item name", () =>{
+        
+    //{force:true}}); elflacogmail.com
+
+});
